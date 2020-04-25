0ed82fc306563b535c61abeaa7240d8f770f8fb5
nick3703 <nick3703@hotmail.com>
2020-04-25 15:59:49 -0500

    25 Apr State Results

--- parametricbyState.R
+++ parametricbyState.R
@@ -80,7 +80,7 @@ rt.func.v3_dusty<-function(dat,mean.Weibull=4.8,sd.Weibull=2.3){
 
 #Read in the daily incidence data from usafacts.org and collect the dates
 #us.data<-read_csv("https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv")
-jhu_all<-read_csv(paste0("https://raw.githubusercontent.com/nick3703/Parametric-Modeling-for-Time-Varying-Reproducibility-Number/master/",lubridate::today(),"_JHU.CSV"))
+jhu_all<-read_csv(paste0("https://raw.githubusercontent.com/nick3703/Parametric-Modeling-for-Time-Varying-Reproducibility-Number/master/",lubridate::today(),"_JHU.csv"))
 #test.data<-us.data%>%filter(stateFIPS > 0)
 #county_pop<-read_csv("county_pop.csv")
 
